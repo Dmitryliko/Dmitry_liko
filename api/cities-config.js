0c64faa91bd004acbd0394c0153a7644ee7bc10f
Anna Volkova <annavolkova@Annas-MacBook-Air.local>
2026-02-05 12:49:58 +0500

    Update cities config: strict msk only, clear other cities

--- api/cities-config.js
+++ api/cities-config.js
@@ -4,106 +4,105 @@ module.exports = {
     '820503': 'msk'
   },
   pageIdToCity: {
-    '41064275': 'barnaul',
-    '24922742': 'blagoveshchensk',
-    '10124546': 'vladivostok',
-    '41064319': 'voronezh',
-    '71257671': 'zelenogradsk',
-    '44474305': 'kaliningrad',
-    '18689756': 'kirov',
-    '24922725': 'krasnodar',
-    '24922728': 'krasnoyarsk',
-    '62181357': 'lipetsk',
-    '10004506': 'msk',
-    '62760995': 'nn',
-    '12247570': 'khabarovsk',
-    '28752366': 'tomsk',
-    '10124533': 'spb',
-    '28199261': 'omsk'
+    "41064275": "barnaul",
+    "24922742": "blagoveshchensk",
+    "10124546": "vladivostok",
+    "41064319": "voronezh",
+    "71257671": "zelenogradsk",
+    "44474305": "kaliningrad",
+    "18689756": "kirov",
+    "24922725": "krasnodar",
+    "24922728": "krasnoyarsk",
+    "62181357": "lipetsk",
+    "10004506": "msk",
+    "62760995": "nn",
+    "12247570": "khabarovsk",
+    "28752366": "tomsk",
+    "10124533": "spb",
+    "28199261": "omsk"
   },
   cities: {
     msk: {
-      apiLogin: '95d5ce46963b47418e5b07543ec77fb4',
-      organizationId: '6fd820ff-65a0-40d6-8309-83d6425aaf2e',
-      terminalGroupId: '16405536-0b9a-30e4-017e-dea16b460064',
+      apiLogin: "95d5ce46963b47418e5b07543ec77fb4",
+      organizationId: "6fd820ff-65a0-40d6-8309-83d6425aaf2e",
+      terminalGroupId: "16405536-0b9a-30e4-017e-dea16b460064",
       paymentTypeKind: 'Card',
       fallbackProductId: '1a87e019-3dff-4e07-b9f1-73cf9140bdbf' // Морс Клюквенный (простой товар, чтобы не было ошибок с размерами)
     },
-    // Заполните данные для других городов ниже по аналогии с msk
-    spb: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
-    },
     barnaul: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     blagoveshchensk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     vladivostok: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     voronezh: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
+    },
+    spb: {
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     zelenogradsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     kaliningrad: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     kirov: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     krasnodar: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     krasnoyarsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     lipetsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     nn: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     khabarovsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     tomsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     },
     omsk: {
-      apiLogin: '',
-      organizationId: '',
-      terminalGroupId: ''
+      apiLogin: "",
+      organizationId: "",
+      terminalGroupId: ""
     }
   }
 };
